412c3008d0d8c69968f641654f15dcce4501de64
Sarfraz Alam <sarfrazk7858@gmail.com>
2024-07-18 14:29:30 +0530

    nextauth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,12 +1,42 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { NextAuthOptions } from "next-auth";
+import NextAuth from "next-auth/next";
 import GoogleProvider from 'next-auth/providers/google'
+import prisma from "@/lib/prisma";
+import { Adapter } from "next-auth/adapters";
 
-export const authOptions:NextAuthOptions={
-    providers:[
+export const authOptions: NextAuthOptions = {
+    adapter: PrismaAdapter(prisma) as Adapter,
+    providers: [
         GoogleProvider({
-            clientId:process.env.GOOGLE_CLIENTID!,
-            clientSecret:process.env.GOOGLE_CLIENTSECRET!,
+            clientId: process.env.GOOGLE_CLIENTID!,
+            clientSecret: process.env.GOOGLE_CLIENTSECRET!,
         })
     ],
-    
+    secret: process.env.NEXTAUTH_SECRET,
+    session: {
+        strategy: "jwt"
+    },
+    callbacks: {
+        async jwt({ token, user }) {
+            if (user && token) {
+                token.id = user.id
+                token.name = user.name
+                token.email = user.email
+                token.picture = user.image
+            }
+            return token
+        },
+        async session({ session, token }) {
+            if (token && session) {
+                session.user.id = token.id
+                session.user.name = token.name
+                session.user.email = token.email
+                session.user.image = token.picture
+            }
+            return session
+        }
+    }
 }
+
+NextAuth(authOptions)
